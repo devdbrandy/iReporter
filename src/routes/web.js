390c75d2b1ab2beda7b618b417f951c8425519d1
Harry Dbrandy <devdbrandy@gmail.com>
2018-11-29 00:10:22 +0100

    Rename routes to web and api, refactor code to es6 standard

---
+++ src/routes/web.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'iReporter' });
+});
+
+export default router;

5eda81f12429f436ed6ea9584f6b3a0320381c6d
Harry Dbrandy <devdbrandy@gmail.com>
2018-11-30 00:57:21 +0100

    Add object models for user and reecord

--- src/models/index.js
+++ src/models/index.js
@@ -1,71 +1,80 @@
-import mocker from 'mocker-data-generator';
+export class User {
+  constructor(attributes) {
+    User.incrementCount();
+    this.id = User.count;
+    this.firstname = attributes.firstname;
+    this.lastname = attributes.lastname;
+    this.othernames = attributes.othernames;
+    this.email = attributes.email;
+    this.phoneNumber = attributes.phoneNumber;
+    this.registered = Date();
+    this.isAdmin = false;
+    this.generateUsername();
+  }
 
-const User = {
-  id: { incrementalId: 1 },
-  firstname: { faker: 'name.firstName' },
-  lastname: { faker: 'name.lastName' },
-  othernames: { faker: 'name.firstName' },
-  email: { faker: 'internet.email' },
-  phoneNumber: { faker: 'phone.phoneNumberFormat' },
-  username: {
-    function: function generateUsername() {
-      return (
-        this.object.lastname.substring(0, 5)
-          + this.object.firstname.substring(0, 3)
-          + Math.floor(Math.random() * 10)
-      );
-    },
-  },
-  registered: { faker: 'date.past' },
-  isAdmin: {
-    function: function randomRole() {
-      return this.faker.random.arrayElement(['true', 'false']);
-    },
-  },
-};
+  generateUsername() {
+    this.username = (
+      this.lastname.substring(0, 5)
+        + this.firstname.substring(0, 3)
+        + Math.floor(Math.random() * 10)
+    );
+  }
 
-const Incident = {
-  id: { incrementalId: 1 },
-  createdOn: { faker: 'date.past' },
-  createdBy: {
-    hasOne: 'user',
-    get: 'id',
-  },
-  type: {
-    function: function randomRole() {
-      return this.faker.random.arrayElement(['red-flag', 'intervention']);
-    },
-  },
-  longitude: {
-    faker: 'address.longitude',
-  },
-  location: {
-    function: function cordinates() {
-      return (
-        `${this.faker.address.latitude()},${this.faker.address.longitude()}`
-      );
-    },
-  },
-  status: {
-    function: function randomRole() {
-      return this.faker.random.arrayElement([
-        'draft', 'under investigation', 'resolved', 'rejected',
-      ]);
-    },
-  },
-  img: { faker: 'image.imageUrl' },
-  images: { static: ['https://via.placeholder.com/650x450', 'https://via.placeholder.com/650x450'] },
-  vidoes: { static: ['https://res.cloudinary.com/devdb/video/upload/v1543497333/sample/video.flv', 'https://res.cloudinary.com/devdb/video/upload/v1543497333/sample/video.flv'] },
-  comment: { faker: 'lorem.paragraph' },
-};
+  update(data) {
+    this.firstname = data.firstname || this.firstname;
+    this.lastname = data.lastname || this.lastname;
+    this.othernames = data.othernames || this.othernames;
+    this.email = data.email || this.email;
+    this.phoneNumber = data.phoneNumber || this.phoneNumber;
+  }
 
-const storage = {};
-mocker()
-  .schema('user', User, 2)
-  .schema('incident', Incident, 3)
-  .build((error, data) => {
-    storage.users = data.user;
-    storage.incidents = data.incident;
-  });
+  adminPrivilege(value) {
+    this.isAdmin = value;
+  }
 
-export default storage;
+  static assignAdmin(user) {
+    user.adminPrivilege(true);
+  }
+
+  static incrementCount() {
+    User.count += 1;
+  }
+}
+
+export class Record {
+  constructor(attributes) {
+    Record.incrementCount();
+    this.id = Record.count;
+    this.creadedOn = Date();
+    this.createdBy = attributes.author;
+    this.type = attributes.type;
+    this.location = attributes.location;
+    this.comment = attributes.comment;
+    this.images = attributes.images;
+    this.videos = attributes.videos;
+    this.status = 'draft';
+  }
+
+  update(data) {
+    if (this.status === 'draft') {
+      this.comment = data.comment || this.comment;
+      this.location = data.location || this.location;
+      this.type = data.type || this.type;
+      this.images = data.images || this.images;
+      this.videos = data.videos || this.videos;
+    }
+
+    return this;
+  }
+
+  publish() {
+    this.status = 'published';
+  }
+
+  static incrementCount() {
+    Record.count += 1;
+  }
+}
+
+User.count = 0;
+Record.count = 0;

24145fa0727f01ad3ca60057eee03aa9f196f292
Harry Dbrandy <devdbrandy@gmail.com>
2018-11-29 01:08:49 +0100

    Add descriptive debug message

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import * as log from 'loglevel';
 import server from './server';
 
-server.listen(3000, () => log.warn('compatible-debug'));
+server.listen(3000, () => log.warn('Listening on port 3000'));

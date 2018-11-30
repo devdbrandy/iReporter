3c18dd570fff87c88a2ed37146694dcce036e59a
Harry Dbrandy <devdbrandy@gmail.com>
2018-11-30 01:21:28 +0100

    Revert "Feature/get list of redflags"

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,15 +1,12 @@
 import express from 'express';
-import dbStorage from '../models/mock';
 
 const router = express.Router();
 
-/* GET list of incidents. */
-router.get('/red-flags', (req, res, next) => {
-  res.status(200)
-    .json({
-      status: 200,
-      data: dbStorage.records,
-    });
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    status: 'OK',
+  });
 });
 
 export default router;

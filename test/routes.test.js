0e8eb5d8636944026c25ce89b0f6087118ad3e59
Harry Dbrandy <devdbrandy@gmail.com>
2018-11-30 05:25:14 +0100

    Implement user authentication with jwt

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,14 +1,37 @@
 import request from 'supertest';
 import chai from 'chai';
+import dotenv from 'dotenv';
 import app from '../src/server';
+import mock from '../src/models/mock';
 
+dotenv.config();
 const should = chai.should();
 
 describe('API Routes', () => {
+  describe('/api/auth', () => {
+    it('authenticates user', (done) => {
+      const user = mock.users[0];
+
+      request(app)
+        .post('/api/auth')
+        .send({
+          username: user.username,
+          password: user.password,
+        })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          res.body.data.should.have.property('token');
+          done(err);
+        });
+    });
+  });
+
   describe('GET /api/users', () => {
     it('retrieve a list of users', (done) => {
       request(app)
         .get('/api/users')
+        .set('Accept', 'application/json')
         .expect(200)
         .end((err, res) => {
           res.body.should.have.property('data')
@@ -24,8 +47,9 @@ describe('API Routes', () => {
         firstname: 'John',
         lastname: 'Doe',
         othernames: 'Posnan',
-        email: 'john@email.com',
         phoneNumber: '622-132-9283',
+        email: 'john@email.com',
+        password: 'abc123',
       };
 
       request(app)

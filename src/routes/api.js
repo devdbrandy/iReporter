e41e22b421c063db3f0d1f7684ee224b8ee3c9ef
Harry Dbrandy <devdbrandy@gmail.com>
2018-11-30 02:29:06 +0100

    Add logic implementation for creating new user

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,24 @@
 import express from 'express';
 import dbStorage from '../models/mock';
+import { User } from '../models';
 
 const router = express.Router();
+
+/* Create new user */
+router.post('/users', (req, res, next) => {
+  const data = req.body;
+  const newUser = new User(data);
+  dbStorage.users.push(newUser);
+
+  res.status(201)
+    .json({
+      status: 201,
+      data: {
+        id: newUser.id,
+        message: 'New user created',
+      },
+    });
+});
 
 /* GET list of incidents. */
 router.get('/red-flags', (req, res, next) => {

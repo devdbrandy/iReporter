1a3024edddd07b336d42a3f244986bbc0a132158
Harry Dbrandy <devdbrandy@gmail.com>
2018-12-01 19:12:49 +0100

    Add error handler middleware

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import createError from 'http-errors';
 import path from 'path';
 import logger from 'morgan';
+import errorHandler from './middleware/error-hander';
 
 import webRouter from './routes/web';
 import apiRouter from './routes/api';
@@ -30,23 +31,6 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  const statusCode = err.status || 500;
-
-  if (req.accepts('json')) {
-    res.json({
-      status: statusCode,
-      error: err.message,
-    });
-  } else {
-    // render the error page
-    res.status(statusCode);
-    res.render('error');
-  }
-});
+app.use(errorHandler);
 
 export default app;

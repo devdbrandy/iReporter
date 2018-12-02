9ef22713dba3a5259249999d0d4bb9c8b6818a24
Harry Dbrandy <devdbrandy@gmail.com>
2018-12-02 01:12:20 +0100

    Implement validation on user route

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,15 +1,55 @@
-export const authValidator = {
-  username: {
-    in: ['body'],
-    errorMessage: 'Username is required',
-    rtrim: [[' ', '-']],
+const validator = {
+  auth: {
+    username: {
+      in: ['body'],
+      errorMessage: 'Username is required',
+      rtrim: { options: [[' ', '-']] },
+    },
+    password: {
+      isLength: {
+        errorMessage: 'Password should be at least 7 chars long',
+        options: { min: 10 },
+      },
+    },
   },
-  password: {
-    isLength: {
-      errorMessage: 'Password should be at least 7 chars long',
-      options: { min: 7 },
+  user: {
+    firstname: {
+      isLength: {
+        errorMessage: 'Firstname is invalid',
+        options: { min: 2 },
+      },
+      rtrim: { options: [[' ', '-']] },
+    },
+    lastname: {
+      isLength: {
+        errorMessage: 'Lastname is invalid',
+        options: { min: 2 },
+      },
+      rtrim: { options: [[' ', '-']] },
+    },
+    email: {
+      errorMessage: 'Provide a valid email address',
+      isEmail: true,
+    },
+    password: {
+      isLength: {
+        errorMessage: 'Password should be at least 7 chars long',
+        options: { min: 7 },
+      },
+    },
+  },
+  record: {
+    location: {
+      errorMessage: 'Invalid coordinates',
+      isLatLong: true,
+    },
+    comment: {
+      isLength: {
+        errorMessage: 'Comment is required',
+        options: { min: 5 },
+      },
     },
   },
 };
 
-export default {};
+export default validator;

d0f510e1e5671132094b33a5bf42785f7b3cd268
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-07 12:08:48 +0000

    Deploy devdbrandy/iReporter to github.com/devdbrandy/iReporter.git:gh-pages

--- js/script.js
+++ js/script.js
@@ -1,42 +1,28 @@
 /* Go Back History */
-const btnBack = document.querySelector('.btn-back');
 const goBack = () => window.history.back();
-if (btnBack) {
-  btnBack.addEventListener('click', goBack);
-}
+const btnBack = document.querySelector('.btn-back');
+btnBack.addEventListener('click', goBack);
 
 /* Listing Uploaded Files */
 const mediaFiles = document.getElementById('mediaFiles');
 const dbTest = document.getElementById('dbtest');
 
-if (mediaFiles) {
-  mediaFiles.addEventListener('change', (e) => {
-    const mediaList = document.querySelector('.media-list');
-    const { files } = e.target;
-    const ul = document.createElement('ul');
+mediaFiles.addEventListener('change', (e) => {
+  const mediaList = document.querySelector('.media-list');
+  const { files } = e.target;
+  const ul = document.createElement('ul');
 
-    for (let i = 0; i < files.length; i += 1) {
-      const li = document.createElement('li');
-      const fileDetails = document.createTextNode(files[i].name);
-      const span = document.createElement('span');
-      const iTag = document.createElement('i');
-      iTag.className = 'fas fa-check';
-      span.appendChild(iTag);
+  for (let i = 0; i < files.length; i += 1) {
+    const li = document.createElement('li');
+    const fileDetails = document.createTextNode(files[i].name);
+    const span = document.createElement('span');
+    const iTag = document.createElement('i');
+    iTag.className = 'fas fa-check';
+    span.appendChild(iTag);
 
-      li.appendChild(fileDetails);
-      li.appendChild(span);
-      ul.appendChild(li);
-    }
-    mediaList.appendChild(ul);
-  });
-}
-
-/* Warning on delete button */
-const removeButton = document.querySelectorAll('.remove');
-if (removeButton.length > 0) {
-  removeButton.forEach((button) => {
-    button.addEventListener('click', () => {
-      alert('Hey Captain, sure you wanna do that?');
-    });
-  });
-}
+    li.appendChild(fileDetails);
+    li.appendChild(span);
+    ul.appendChild(li);
+  }
+  mediaList.appendChild(ul);
+});

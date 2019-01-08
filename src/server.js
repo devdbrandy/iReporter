dbc55f0590682aaf24912ad1075fc50ea519b056
Harry Dbrandy <devdbrandy@gmail.com>
2019-01-08 13:05:19 +0100

    chore(server): follow cors guideline to enable all cors requests

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use(cors());
 app.options('*', cors());
 
 app.use(`/api/${version}`, apiRouter);

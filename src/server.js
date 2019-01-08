4485f88a4d3e5cbe49f3dd1b58509dafe25221f1
Harry Dbrandy <devdbrandy@gmail.com>
2019-01-08 11:15:45 +0100

    feat(server): add support for cors request
    Installed and configured cors middleware options for all routes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import createError from 'http-errors';
 import path from 'path';
 import logger from 'morgan';
+import cors from 'cors';
 import { exceptionHandler } from './middleware';
 import { config } from './utils/helpers';
 
@@ -23,6 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.options('*', cors());
 
 app.use(`/api/${version}`, apiRouter);
 app.use('/auth', authRouter);

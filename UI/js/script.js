ed36b189022c7359cbcd3446ae9896429146745a
Harry Dbrandy <devdbrandy@gmail.com>
2019-01-11 14:44:23 +0100

    :sparkles: (dashboard): consume api for displaying list of records
    - Implement functionality for displaying list of records on
    user dashboard
    - Enhanced UI experience with dialog support
    
    [Delivers #163032939]

--- UI/js/script.js
+++ UI/js/script.js
@@ -91,6 +91,160 @@ async function initMap(cordinates) {
 }
 /* End Initialize Map */
 
+/* RecordAPI class */
+class RecordAPI {
+  static get uri() {
+    return `${getEnv('API_URI')}/api/v1`;
+  }
+
+  static async fetchRecords(type = '') {
+    const { token } = auth();
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await fetch(`${RecordAPI.uri}/${type}`, options);
+    const { data } = await response.json();
+    return data;
+  }
+
+  static fetchRecord(type, id) {}
+
+  static deleteRecord(type, id) {}
+}
+
+/* UI class */
+class UI {
+  static async showRecords() {
+    if (getPath() !== 'dashboard.html') {
+      return false;
+    }
+
+    // TODO: show preloader
+
+    try {
+      const records = await RecordAPI.fetchRecords('red-flags');
+      if (records) {
+        // TODO: hide preloader
+        return records.forEach(record => UI.addRecordToList(record));
+      }
+      return true;
+    } catch (err) {
+      UI.snackbar('Unable to fetch data, retry in 10 secs...');
+      return setTimeout(() => {
+        UI.showRecords();
+      }, 10000);
+    }
+  }
+
+  static addRecordToList(record) {
+    const list = document.getElementById('record-list');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td class="title">${record.title}</td>
+      <td><span class="tag tag-${record.type}">${record.type}</span></td>
+      <td>${record.createdOn}</td>
+      <td><span class="tag">${record.status}</span></td>
+      <td>
+        <div class="wrapper">
+          <button class="btn btn-info action-btn view"><i class="far fa-eye"></i>
+            <span class="text">View</span>
+          </button>
+          <a href="edit-record.html" class="btn btn-success action-btn edit">
+            <i class="far fa-edit"></i>
+            <span class="text">Edit</span>
+          </a>
+          <button class="btn btn-danger action-btn delete"><i class="far fa-trash-alt"></i>
+            <span class="text">Delete</span>
+          </button>
+        </div>
+      </td>`;
+    row.setAttribute('data-record', JSON.stringify(record));
+    list.appendChild(row);
+
+    row.querySelector('button.view').addEventListener('click', (e) => {
+      UI.showRecord(row);
+    });
+
+    row.querySelector('button.delete').addEventListener('click', (e) => {
+      UI.removeRecord(row);
+    });
+  }
+
+  static async showRecord(el) {
+    const {
+      title,
+      comment,
+      location,
+    } = JSON.parse(el.getAttribute('data-record'));
+
+    const modalBody = document.querySelector('.modal--body');
+    modalBody.querySelector('.title h4').innerText = title;
+    modalBody.querySelector('.comment').innerText = comment;
+
+    const place = await initMap(location);
+    modalBody.querySelector('.location .text').innerText = place;
+    openModal();
+  }
+
+  static removeRecord(el) {
+    console.log('REMOVE RECORD');
+  }
+
+  static goBack() {
+    window.history.back();
+  }
+
+  static listMedaiUpload() {
+    const mediaList = document.querySelector('.media-list');
+    const { files } = e.target;
+    const ul = document.createElement('ul');
+
+    for (let i = 0; i < files.length; i += 1) {
+      const li = document.createElement('li');
+      const fileDetails = document.createTextNode(files[i].name);
+      const span = document.createElement('span');
+      const iTag = document.createElement('i');
+      iTag.className = 'fas fa-check';
+      span.appendChild(iTag);
+
+      li.appendChild(fileDetails);
+      li.appendChild(span);
+      ul.appendChild(li);
+    }
+    mediaList.appendChild(ul);
+  }
+
+  static snackbar(text, ms = 3000) {
+    const snackbar = UI.createSnackbar(text);
+
+    document.body.appendChild(snackbar);
+    setTimeout(() => {
+      snackbar.classList.remove('show');
+    }, ms);
+  }
+
+  static createSnackbar(text) {
+    const div = document.createElement('div');
+    div.setAttribute('id', 'snackbar');
+    div.innerText = text;
+    div.classList.add('show');
+    return div;
+  }
+}
+
+/* Event: Load list of records */
+document.addEventListener('DOMContentLoaded', UI.showRecords);
+
+/* Go Back History */
+const btnBack = document.querySelector('.btn-back');
+if (btnBack) {
+  btnBack.addEventListener('click', UI.goBack);
+}
+
 /* Listing Uploaded Files */
 const mediaFiles = document.getElementById('media-files');
 if (mediaFiles) {
